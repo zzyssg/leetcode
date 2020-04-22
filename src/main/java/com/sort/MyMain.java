1fca8e5da8f8a4026bdfee08c42ccc987b9a88b6
zzyssg <zy1704528@buaa.edu.cn>
2020-04-22 13:54:55 +0800

    Main

--- src/main/java/com/sort/MyMain.java
+++ src/main/java/com/sort/MyMain.java
@@ -14,14 +14,45 @@ public class MyMain {
 //        Sort sort = new ShellSort();
 //        Sort sort = new ChooseSort();
 //        Sort sort = new HeapSort();
-        Sort sort = new MergeSort();
-        sort.sort(nums);
+//        Sort sort = new MergeSort();
+//        sort.sort(nums);
+        sortNums(nums);
         System.out.println();
-        for (int i = 0; i < 9; i++) {
-            if (nums[i] > nums[i + 1]) {
-                System.out.println(false);
-            }
+        for (int i = 0; i < 10; i++) {
+//            if (nums[i] > nums[i + 1]) {
+//                System.out.println(false);
+//            }
+            System.out.print(nums[i] + " ");
         }
-//        bubbleSort
+
+    }
+
+    private static void sortNums(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+    }
+
+    private static void quickSort(int[] nums, int i, int j) {
+        if(i >= j) return;
+        int p = partation(nums, i, j);
+        quickSort(nums, i, p - 1);
+        quickSort(nums, p + 1, j);
+
+    }
+
+    private static int partation(int[] nums, int start, int end) {
+        int i = start - 1, j = end;
+        int base = nums[end];
+        while (true) {
+            while(--j >= start && nums[j] < base);
+            while (++i <= end && nums[i] > base) ;
+            if(i >= j) break;
+            int tem1 = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tem1;
+        }
+        int tem2 = nums[end];
+        nums[end] = nums[i];
+        nums[i] = tem2;
+        return i;
     }
 }
