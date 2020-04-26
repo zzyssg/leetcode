a606e90157f79c2a3f43cd16c797b1dea0aa3a7d
zzyssg <zy1704528@buaa.edu.cn>
2020-04-26 20:55:58 +0800

    heapsort

--- src/main/java/com/sort/HeapSort.java
+++ src/main/java/com/sort/HeapSort.java
@@ -1,50 +1,51 @@
 package com.sort;
 
 public class HeapSort extends Sort {
+
+
     @Override
     void sort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
+            //大根堆
             heapInsert(nums, i);
         }
-        for(int i = nums.length - 1;i >= 0;i--){
+        for (int i = nums.length - 1; i >= 0; i--) {
             swap(nums, 0, i);
+            //调整
             heapify(nums, 0, i);
         }
     }
 
     private void heapify(int[] nums, int i, int j) {
+        int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
-        int largest = i;
         while (left < j) {
-            if (nums[left] > nums[i]) {
+            if (nums[i] < nums[left]) {
                 largest = left;
             }
-            if (right < j && nums[right] > nums[largest]) {
+            if (right < j && nums[largest] < nums[right]) {
                 largest = right;
             }
-            if (largest == i) break;
+            if (i == largest) break;
             swap(nums, largest, i);
             i = largest;
             left = 2 * i + 1;
             right = 2 * i + 2;
-
         }
 
     }
 
     private void heapInsert(int[] nums, int i) {
-        int parent = 0;
+        int parent;
         while (i != 0) {
             parent = (i - 1) / 2;
-            if (nums[i] > nums[parent]) {
-                swap(nums, i, parent);
+            if (nums[parent] < nums[i]) {
+                swap(nums, parent, i);
                 i = parent;
             } else {
                 break;
             }
         }
-
     }
-
 }
